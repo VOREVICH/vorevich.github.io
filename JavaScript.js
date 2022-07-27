05850e4670dbb877936c60fc7221c9a5dd4ce4d3
Vladimir <@gagarin.tor@gmail.com>
2022-07-27 17:55:30 +0300

    vldmr

---
+++ JavaScript.js
@@ -0,0 +1,14 @@
+const accordions = document.querySelectorAll('.accordion-item');
+
+for(item of accordions) {
+  item.addEventListener('click', function() {
+    if(this.classList.contains("active")) {
+      this.classList.remove('active');
+    }else {
+      for(el of accordions) {
+        el.classList.remove('active');
+      }
+      this.classList.add('active');
+    }
+  })
+}
