dfbc425a765e46d8b8c8573902e3b61dda9161db
Vladimir <@gagarin.tor@gmail.com>
2022-08-04 12:21:54 +0300

    'lol'

---
+++ .vscode/JavaScript.js
@@ -0,0 +1,93 @@
+const accordions = document.querySelectorAll('.accordion-item');
+
+for(item of accordions) {
+  item.addEventListener('click', function() {
+    if(this.classList.contains("active")) {
+      this.classList.remove('active');
+    }else {
+      for(el of accordions) {
+        el.classList.remove('active');
+      }
+      this.classList.add('active');
+    }
+  })
+}
+const panels = document.querySelectorAll('.panel');
+panels.forEach(panel => {
+    panel.addEventListener('click', () => {
+        removeActiveClasses();
+        panel.classList.add('active');
+    });
+});
+function removeActiveClasses() {
+    panels.forEach(panel => {
+        panel.classList.remove('active');
+    });
+}
+
+
+
+const sliderLine = document.querySelector('.slider-line'),
+      prevbutton = document.querySelector('.button-prev'),
+      nextbutton = document.querySelector('.button-next'),
+      dots = document.querySelectorAll(".dot"),
+      Carusel = document.querySelector('.carusel');
+let position = 0,
+    dotIndex = 0;
+
+
+const nextSlide = () => {
+  if(position < (dots.length - 1) * Carusel.width) {
+     position += Carusel.width
+     dotIndex++
+  }else {
+    position = 0
+    dotIndex = 0
+  }
+      sliderLine.style.left = -position +'px'
+      thisSlide(dotIndex)
+}
+
+
+const prevSlide = () => {
+  if(position > 0) {
+     position -= Carusel.width
+     dotIndex--
+  }else {
+    position = (dots.length - 1) * Carusel.width
+    dotIndex = (dots.length - 1)
+  }
+      sliderLine.style.left = -position +'px'
+      thisSlide(dotIndex)
+}
+
+const thisSlide = (Index) => {
+  for(let dot of dots) {
+    dot.classList.remove('activ')
+  }
+  dots[Index].classList.add('activ')
+}
+
+
+
+
+//----------------------------Eventlistenner--------------------------------
+prevbutton.addEventListener('click', prevSlide)
+nextbutton.addEventListener('click', nextSlide)
+dots.forEach((dot, Index) =>{
+  dot.addEventListener('click', () => {
+    position = Carusel.width * Index
+    sliderLine.style.left = -position +'px'
+    dotIndex = Index
+    thisSlide(dotIndex)
+  
+  
+  })
+})
+//-------------------Интервал-------------------------------
+makeTimer();
+function makeTimer(){
+   clearInterval(timer)
+   timer = setInterval(() => {
+    nextSlide()
+  }, 6000);}
